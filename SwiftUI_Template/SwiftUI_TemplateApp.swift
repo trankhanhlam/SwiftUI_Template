8146fa18eb9537eda0ce0f51d7e213321cf54b9a
lamtk <lamtrankhanh94@gmail.com>
2024-04-16 08:45:26 +0700

    Initial Commit

---
+++ SwiftUI_Template/SwiftUI_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TemplateApp.swift
+//  SwiftUI_Template
+//
+//  Created by  Trần Khánh Lâm on 16/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
